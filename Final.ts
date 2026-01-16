a4877111a2ba4f0f21126005243d91e25cf63507
Connor-Wilkins <connor.wilkins@stu.ocsb.ca>
2026-01-16 20:01:03 +0000

    variables, constants

---
+++ Final.ts
@@ -0,0 +1,12 @@
+// Author: Connor Wilkins
+// Version: 1.0.0
+// Date: 2026-1-14
+
+// Constants
+
+const POINTS_PER_CORRECT: number = 1;
+
+// Variables
+let score: number = 0;
+
+// Question type
